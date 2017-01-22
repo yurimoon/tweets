f36872e5996e65f9cf7007ba0abec5284c1943bf
yurimoon <sky.ac3ce@gmail.com>
2017-01-22 08:52:45 +0000

    update database & schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20170122053723) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tweets", force: :cascade do |t|
     t.text     "content"
     t.datetime "created_at", null: false
